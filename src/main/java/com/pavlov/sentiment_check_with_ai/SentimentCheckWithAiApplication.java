58a8c0e8f65a6a44b1dcd43d60c69f53e4410d51
Pavlov Kirill <pochta.navsegda@mail.ru>
2026-05-04 14:31:38 +0300

    Init

---
+++ src/main/java/com/pavlov/sentiment_check_with_ai/SentimentCheckWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.pavlov.sentiment_check_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentCheckWithAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentCheckWithAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavlov/sentiment_check_with_ai/SentimentCheckWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlov.sentiment_check_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentCheckWithAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
